5e43f48819fc243ad72e4842d96ab4d80e8fa0d6
Gabriel <gabrielantuneslima10@gmail.com>
2024-12-09 01:44:58 -0300

    Please enter the commit message for your changes. Lines starting 	deleted:    assets/bg-desktop.jpg 	deleted:    assets/bg-mobile.jpg 	modified:   index.html 	modified:   script.js 	modified:   style.css 	deleted:    tabela.html

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function toggleMode() {
-  //dentro do comento eu acesso minha tag html
-  const html = document.documentElement
-  
-  html.classList.toggle("light")
-  //o toggle tem a logica semelhante a condiçao/logica a baixo, se tiver o 'light' ele tira e se não tiver ele coloca!
-  
-  /*
-  if (html.classList.contains("light")) {  // se no html as listas de classes, estiver'light'
-  html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-}
-*/
-
-
-}
-
 const mainMenu = document.querySelector(".mainMenu")
 const tabelaMenu = document.querySelector(".tabelaMenu")
 
